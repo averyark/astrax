d111853a0ae77513f8285982080a92b6d3640229
DevArke <90465200+DevArke@users.noreply.github.com>
2022-09-11 22:10:32 +0800

    tween template v1

--- src/utilities/tween.lua
+++ src/utilities/tween.lua
@@ -96,6 +96,341 @@ tween.instance = function(
 	return _tween
 end
 
+-- TODO: tweenSequence.andThenRepeatUntil
+
+local tweenSequenceTemplate = {}
+local tweenSequence = {}
+
+--[[
+	Sequence: Pause the sequence until the given time has passed. Note: The time unit is in seconds.
+	Waits for the previous tween to finish.
+
+	@chainable true
+	@yield true
+	```lua
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThenWait(5)
+		:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+	```
+]]
+tweenSequenceTemplate.andThenWait = function(self: TweenSequenceTemplate, n: number): TweenSequenceTemplate
+	table.insert(self._process, { fType = "andThenWait", n = n })
+	return self
+end
+tweenSequence.__exec__andThenWait = function(self: Tweenable, args: { n: number })
+	assert(t.numberPositive(args.n), "[tween] Seconds must be a positive number")
+	self:__exec__andThen()
+	task.wait(args.n)
+end
+
+--[[
+	Sequence: Proceeds only if the callback returns true, otherwise the process is stopped.
+	Waits for the previous tween to finish.
+
+	@chainable true
+	@yield false
+	```lua
+	local conditionFunc = function(self)
+		if condition then -- some condition here
+			return true
+		end
+	end
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThenIf(conditionFunc) -- Process stops here if conditionFunc returns false
+		:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+	```
+]]
+tweenSequenceTemplate.andThenIf = function(self: TweenSequenceTemplate, f: (...any) -> (boolean)): TweenSequenceTemplate
+	table.insert(self._process, { fType = "andThenIf", conditionF = f })
+	return self
+end
+tweenSequence.__exec__andThenIf =
+	function(self: Tweenable, args: { conditionF: (...any) -> (boolean) }, cancel: () -> ())
+		assert(t.callback(args.conditionF), "[tween] Condition must be a callback")
+		self:__exec__andThen()
+		if not args.conditionF(self) then
+			cancel()
+		end
+	end
+
+--[[
+	Sequence: Yields the thread until the callback returns true, then continues the sequence. Note: Use this function carefully as it could cause the sequence to never end. 
+	Waits for the previous tween to finish.
+
+	@chainable true
+	@yield true
+	```lua
+	local conditionFunc = function(self)
+		if condition then -- some condition here
+			return true
+		end
+	end
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThenWaitUntil(conditionFunc)
+		:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+	```
+]]
+tweenSequenceTemplate.andThenWaitUntil =
+	function(self: TweenSequenceTemplate, f: (...any) -> (boolean)): TweenSequenceTemplate
+		table.insert(self._process, { fType = "andThenWaitUntil", conditionF = f })
+		return self
+	end
+tweenSequence.__exec__andThenWaitUntil = function(self: Tweenable, args: { conditionF: (...any) -> (boolean) })
+	assert(t.callback(args.conditionF), "[tween] Condition must be a callback")
+	self:__exec__andThen()
+	repeat
+		task.wait()
+	until args.conditionF(self)
+end
+
+--[[
+	Sequence: Proceeds after the last tween has finished.
+
+	@chainable true
+	@yield true
+
+	```lua
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThen()
+		:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+	```
+]]
+tweenSequenceTemplate.andThen = function(self: TweenSequenceTemplate): TweenSequenceTemplate
+	table.insert(self._process, { fType = "andThen" })
+	return self
+end
+tweenSequence.__exec__andThen = function(self: Tweenable, args: {})
+	self.tween:Wait()
+end
+
+--[[
+	Sequence: Animating the object. Note: This function will not yield, use :andThen() if you want to wait for the animation to finish before executing the next process
+
+	@chainable true
+	@yield false
+
+	```lua
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThen()
+		:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+	```
+]]
+tweenSequenceTemplate.play = function(
+	self: TweenSequenceTemplate,
+	goals: { [string]: any },
+	duration: number?,
+	easingStyle: easingStyles?,
+	easingDirection: easingDirections?
+): TweenSequenceTemplate
+	table.insert(
+		self._process,
+		{
+			fType = "play",
+			goals = goals,
+			duration = duration,
+			easingStyle = easingStyle,
+			easingDirection = easingDirection,
+		}
+	)
+	return self
+end
+tweenSequence.__exec__play = function(
+	self: Tweenable,
+	args: {
+		goals: { [string]: any },
+		duration: number?,
+		easingStyle: easingStyles?,
+		easingDirection: easingDirections?,
+	}
+)
+	self.tween = BoatTween:Create(self.object, {
+		Time = args.duration or 0.3,
+		EasingStyle = args.easingStyle or "Cubic",
+		EasingDirection = args.easingDirection or "Out",
+		Goal = args.goals,
+	})
+	self.tween:Play()
+end
+
+--[[
+	Sequence: Resets the object to its original state. Default must be set with :default({}) before using this function.
+
+	@chainable true
+	@yield false
+
+	```lua
+	tweenSequence.template()
+		:default({
+			Position = UDim2.new(0, 0, 0, 0)
+		})
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThenWait(2)
+		:reset()
+	```
+]]
+tweenSequenceTemplate.reset = function(self: TweenSequenceTemplate): TweenSequenceTemplate
+	table.insert(self._process, { fType = "reset" })
+	return self
+end
+tweenSequence.__exec__reset = function(self: Tweenable, args: {})
+	assert(t.table(self.default), "[tween] Default values must be a table")
+	for a, b in pairs(self.default) do
+		self.object[a] = b
+	end
+end
+
+--[[
+	Sequence: Resets the object to its original state. Default must be set with :default({}) before using this function.
+
+	@chainable true
+	@yield false
+
+	```lua
+	tweenSequence.template()
+		:default({
+			Position = UDim2.new(0, 0, 0, 0)
+		})
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThenWait(2)
+		:reset()
+	```
+]]
+tweenSequenceTemplate.default = function(self: TweenSequenceTemplate, default: { [string]: any }): TweenSequenceTemplate
+	table.insert(self._process, { fType = "default", default = default })
+	return self
+end
+tweenSequence.__exec__default = function(self: Tweenable, args: { default: { [string]: any } })
+	assert(t.table(args.default), "[tween] Default values must be a table")
+	self.default = args.default
+end
+
+--[[
+	Sequence: Animating the object. Note: This function will not yield, use :andThen() if you want to wait for the animation to finish before executing the next process
+
+	@chainable true
+	@yield false
+
+	```lua
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+		:andThen()
+		:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+	```
+]]
+tweenSequenceTemplate.playDefault = function(
+	self: TweenSequenceTemplate,
+	duration: number?,
+	easingStyle: easingStyles?,
+	easingDirection: easingDirections?
+): TweenSequenceTemplate
+	table.insert(
+		self._process,
+		{ fType = "playDefault", duration = duration, easingStyle = easingStyle, easingDirection = easingDirection }
+	)
+	return self
+end
+tweenSequence.__exec__playDefault = function(
+	self: Tweenable,
+	args: {
+		duration: number?,
+		easingStyle: easingStyles?,
+		easingDirection: easingDirections?,
+	}
+)
+	assert(t.table(self.default), "[tween] Default values must be a table")
+	self.tween = BoatTween:Create(self.object, {
+		Time = args.duration or 0.3,
+		EasingStyle = args.easingStyle or "Cubic",
+		EasingDirection = args.easingDirection or "Out",
+		Goal = self.default,
+	})
+	self.tween:Play()
+end
+
+--[[
+	Creates a chainable TweenSequenceTemplate object, you can chain multiple sequence
+
+	@chainable true
+	@yield false
+	```lua
+	tweenSequence.template()
+		:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+	```
+]]
+tween.template = function(): TweenSequenceTemplate
+	local self = {
+		_process = {},
+	}
+
+	return setmetatable(self, {
+		__index = tweenSequenceTemplate,
+	})
+end
+
+--[[
+	Plays the sequence
+
+	@yield #
+	```lua
+	local sequence = tweenSequence.template()
+			:play({ Position = UDim2.new(0, 0, 1, 0) }, 0.5)
+			:andThen()
+			:play({ Position = UDim2.new(0, 0, 0, 0) }, 0.5)
+
+	tween.run(sequence, GuiFrame)
+	```
+]]
+tween.run = function(tsTemplate: TweenSequenceTemplate, object: Instance): Tweenable
+	local self = setmetatable({
+		template = tsTemplate,
+		object = object,
+		processes = {},
+	}, {
+		__index = tweenSequence,
+	})
+	for _, process in pairs(self.template._process) do
+		table.insert(self.processes, process)
+	end
+	for _, fData in pairs(self.processes) do
+		local cancelled = false
+		tweenSequence["__exec__" .. fData.fType](self, fData, function()
+			cancelled = true
+		end)
+		if cancelled then
+			break
+		end
+	end
+	return self
+end
+
+type Tweenable = typeof(tween.run(tween.template(), Instance.new("Frame")))
+type TweenSequenceTemplate = typeof(tween.template()) & {
+	andThen: (self: TweenSequenceTemplate) -> TweenSequenceTemplate,
+	andThenWait: (self: TweenSequenceTemplate, seconds: number) -> TweenSequenceTemplate,
+	andThenWaitUntil: (self: TweenSequenceTemplate, condition: () -> (boolean)) -> TweenSequenceTemplate,
+	andThenIf: (self: TweenSequenceTemplate, condition: () -> (boolean)) -> TweenSequenceTemplate,
+	play: (
+		self: TweenSequenceTemplate,
+		goals: { [string]: any },
+		duration: number?,
+		easingStyle: easingStyles?,
+		easingDirection: easingDirections?
+	) -> TweenSequenceTemplate,
+	reset: () -> TweenSequenceTemplate,
+	default: (self: TweenSequenceTemplate, { [string]: any }) -> TweenSequenceTemplate,
+	playDefault: (
+		self: TweenSequenceTemplate,
+		duration: number?,
+		easingStyle: easingStyles?,
+		easingDirection: easingDirections?
+	) -> TweenSequenceTemplate,
+}
+
 -- server syncing tween
 tween.tweenClientInstance = function() end
 
